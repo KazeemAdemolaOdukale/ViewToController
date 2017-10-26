fac7c352070cce7ffc5f73a108d13e5faa0b30de
KazeemAdemolaOdukale <kaodukale@gmail.com>
2017-10-25 20:08:29 -0400

    form

---
+++ ViewToController/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewToController.Models;
+
+namespace ViewToController.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDbContext db = new StudentDbContext();
+
+        // GET: Student
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                List<Student> studs = db.Students.Where(y => y.Id == student.Id).ToList();
+                if (studs.Count > 0)
+                {
+                    ViewBag.Duplicate = "Record already exist";
+                }
+                else
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    ViewBag.SuccessMessage = "Record successfully added";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ ViewToController/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ViewToController.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string CourseOne { get; set; }
+        public string CourseTwo { get; set; }
+        public string CourseThree { get; set; }
+    }
+}
